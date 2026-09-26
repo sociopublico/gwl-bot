36fb86407907ac097e660c94dd8a69462583609d
Agustina Nahas <achinahas95@gmail.com>
2026-09-26 09:52:17 -0300

    speakers 26

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
-pipeline/.venv/bin/python -m pipeline publish --session 81 --day 2026-09-24 --sheet  --github
-pipeline/.venv/bin/python -m pipeline coding --session 81 --day 2026-09-24
-pipeline/.venv/bin/python -m pipeline coding-sheet --session 81 --day 2026-09-24
+pipeline/.venv/bin/python -m pipeline roster --session 81 --day 2026-09-25 
+pipeline/.venv/bin/python -m pipeline fetch --session 81 --day 2026-09-25 --skip-existing
+pipeline/.venv/bin/python -m pipeline publish --session 81 --day 2026-09-25 --sheet  --github
+pipeline/.venv/bin/python -m pipeline coding --session 81 --day 2026-09-25
+pipeline/.venv/bin/python -m pipeline coding-sheet --session 81 --day 2026-09-25
